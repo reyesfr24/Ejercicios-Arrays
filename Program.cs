38200dbbf650eb8828c643e777a52ef1345fb160
reyesfr24 <130443772+reyesfr24@users.noreply.github.com>
2023-04-11 10:14:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Programaciondesde1_EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe cinco numeros:");
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int mayor = 0;
+            int menor = numeros[0];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+            }
+
+            Console.WriteLine("El numero mas grande es: {0}", mayor);
+            Console.WriteLine("El numero mas pequenio es: {0}", menor);
+        }
+    }
+}
